0ad28d85fd898619a46d60458709cfb1e4582425
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-05-14 17:40:08 -0300

    fix midleware error as json, status code

--- src/midlewares/error-as-json.js
+++ src/midlewares/error-as-json.js
@@ -2,8 +2,7 @@ module.exports = app => {
   app.use((err, req, res, next) => {
     console.error('>>>>>> midleware error-as-json', err)
     if (err) {
-      const { message } = err
-      return res.status(500).json({ message })
+      return res.status(err.status || 500).json({ message: err.message })
     }
 
     next()
