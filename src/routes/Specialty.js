f73542ffee4095a8d32c2a3fe98941b2cb65a811
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-05-15 14:05:58 -0300

    drop specialty

--- src/routes/Specialty.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-  app.get('/specialties', app.controllers.Specialty.index)
-  app.get('/specialties/:id', app.controllers.Specialty.get)
-  app.post('/specialties', app.controllers.Specialty.store)
-  app.put('/specialties', app.controllers.Specialty.update)
-  app.delete('/specialties/:id', app.controllers.Specialty.delete)
-}
