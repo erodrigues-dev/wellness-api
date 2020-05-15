e8c9a699e4ecf6c85952a588abbf62349aa46e0f
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-05-15 15:31:43 -0300

    feature: Activity validate, routes and contoller

--- src/midlewares/validates/Activity.js
+++ src/midlewares/validates/Activity.js
@@ -1,10 +1,43 @@
 const { celebrate, Joi, Segments } = require('celebrate')
 
 module.exports = app => {
+  app.get(
+    '/activities',
+    celebrate({
+      [Segments.QUERY]: Joi.object().keys({
+        name: Joi.string().allow('').optional(),
+        page: Joi.number().min(1).optional(),
+        limit: Joi.number().min(1).optional()
+      })
+    })
+  )
+
+  app.get(
+    '/activities/:id',
+    celebrate({
+      [Segments.PARAMS]: Joi.object({
+        id: Joi.number().required()
+      })
+    })
+  )
+
   app.post(
     '/activities',
     celebrate({
       [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        description: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        duration: Joi.number().integer().required()
+      })
+    })
+  )
+
+  app.put(
+    '/activities',
+    celebrate({
+      [Segments.BODY]: Joi.object().keys({
+        id: Joi.number().min(1).required(),
         name: Joi.string().required(),
         description: Joi.string().required(),
         price: Joi.number().precision(2).required(),
