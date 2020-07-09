572ccbb1b1802166283defd0102087a2b94f75c7
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-07-09 12:23:42 -0300

    cli utilitary for create and compare password

---
+++ password.js
@@ -0,0 +1,36 @@
+const util = require('./src/utils/hash-password')
+
+const [, , command, arg1, arg2] = process.argv
+
+if (command === 'create' && arg1) {
+  util.hash(arg1).then(hash => {
+    console.log(`
+      >>> create:\n
+      > pwd: ${arg1}
+      > hash: ${hash}
+    `)
+  })
+} else if (command === 'compare' && arg1 && arg2) {
+  util.compare(arg1, arg2).then(match => {
+    console.log(`
+      >>> compare:\n
+      > pwd: ${arg1}
+      > hash: ${arg2}
+      > match: ${match}
+    `)
+  })
+} else {
+  console.log(`
+    >>>HELP<<<
+
+    Commands:
+
+    -> create:\t\thash password
+    ex.: $ node password create 123
+
+    -> compare:\t\tcompare password
+    ex.: $ node password compare 12345678 '$2b$08$wTb/piIOc/Ntc96Pm6LoTeDtQgF9p3nPGle8LuvQUQw/lZURAj0oy'
+
+    Tips: Use single quotes "'" in the password and hash
+  `)
+}
