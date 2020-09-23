aa45f844ad110348e00491affc25d83b0aeeb3bd
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-09-23 13:26:25 -0300

    fix permissions

--- src/admin/midlewares/permissions/Activity.ts
+++ src/admin/midlewares/permissions/Activity.ts
@@ -4,6 +4,8 @@ import { ACTIONS, checkPermission } from '../../../shared/utils/permission';
 
 const router = Router();
 
+router.get('/activities', checkPermission('activities', ACTIONS.LIST));
+router.get('/activities/:id', checkPermission('activities', ACTIONS.LIST));
 router.post('/activities', checkPermission('activities', ACTIONS.CREATE));
 router.put('/activities', checkPermission('activities', ACTIONS.UPDATE));
 
