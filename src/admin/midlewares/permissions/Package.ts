1380d00ec5fa435218929acbcf28fdb76afaf04a
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-09-24 20:49:43 -0300

    fix permissions

--- src/admin/midlewares/permissions/Package.ts
+++ src/admin/midlewares/permissions/Package.ts
@@ -3,6 +3,8 @@ import { ACTIONS, checkPermission } from '../../../shared/utils/permission';
 
 const router = Router();
 
+router.get('/packages', checkPermission('packages', ACTIONS.LIST));
+router.get('/packages/:id', checkPermission('packages', ACTIONS.LIST));
 router.post('/packages', checkPermission('packages', ACTIONS.CREATE));
 router.put('/packages', checkPermission('packages', ACTIONS.UPDATE));
 
