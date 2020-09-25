ef035dab1a89ea49c8b3fa08ce0d0c72c3ca91f3
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-09-25 10:17:26 -0300

    fix activity association with employee

--- src/shared/database/models/Activity.ts
+++ src/shared/database/models/Activity.ts
@@ -1,6 +1,7 @@
-import { Sequelize, Model, DataTypes } from 'sequelize';
+import { Sequelize, Model, DataTypes, Association } from 'sequelize';
 
 import IActivity from '../../models/IActivity';
+import Employee from './Employee';
 
 export default class Activity extends Model<IActivity> implements IActivity {
   id?: number;
@@ -11,8 +12,14 @@ export default class Activity extends Model<IActivity> implements IActivity {
   imageUrl: string;
   employeeId: number;
 
+  employee?: Employee;
+
   readonly createdAt: Date;
   readonly updatedAt: Date;
+
+  static associations: {
+    employee: Association<Activity, Employee>;
+  };
 
   static setup(connection: Sequelize) {
     Activity.init(
@@ -28,5 +35,10 @@ export default class Activity extends Model<IActivity> implements IActivity {
     );
   }
 
-  static setupAssociations() {}
+  static setupAssociations() {
+    Activity.belongsTo(Employee, {
+      foreignKey: 'employeeId',
+      as: 'employee'
+    });
+  }
 }
