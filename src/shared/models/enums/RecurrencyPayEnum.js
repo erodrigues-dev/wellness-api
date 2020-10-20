eaaeee39894ce168c05e131b94e9b29eb73005a0
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-10-20 17:46:23 -0300

    remove js wrong build

--- src/shared/models/enums/RecurrencyPayEnum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.RecurrencyPayEnum = void 0;
-var RecurrencyPayEnum;
-(function (RecurrencyPayEnum) {
-    RecurrencyPayEnum["oneTime"] = "one-time";
-    RecurrencyPayEnum["weekly"] = "weekly";
-    RecurrencyPayEnum["monthly"] = "monthly";
-})(RecurrencyPayEnum = exports.RecurrencyPayEnum || (exports.RecurrencyPayEnum = {}));
