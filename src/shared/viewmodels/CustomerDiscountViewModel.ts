4b0235402759e4ba846bad4534852d17432ad03e
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-10-21 21:36:51 -0300

    create view-model

---
+++ src/shared/viewmodels/CustomerDiscountViewModel.ts
@@ -0,0 +1,33 @@
+export default class CustomerDiscountViewModel {
+  id: number;
+  relationId: number;
+  relationName: string;
+  relationType: string;
+  type: string;
+  value: number;
+  customerId: number;
+  customerName: string;
+  userId: number;
+  userName: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  static from(obj: any): CustomerDiscountViewModel {
+    const view = new CustomerDiscountViewModel();
+
+    view.id = obj.id;
+    view.relationId = obj.relation_id;
+    view.relationName = obj.relation_name;
+    view.relationType = obj.relation_type;
+    view.type = obj.type;
+    view.value = Number(obj.value);
+    view.customerId = obj.customer_id;
+    view.customerName = obj.customer_name;
+    view.userId = obj.user_id;
+    view.userName = obj.user_name;
+    view.createdAt = obj.created_at;
+    view.updatedAt = obj.updated_at;
+
+    return view;
+  }
+}
