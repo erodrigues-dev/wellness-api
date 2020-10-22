3bbeb071c12ebd5e1954f724df26fe43f756c5a4
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-10-21 21:37:07 -0300

    change routes

--- src/admin/routes/CustomerDiscountRouter.ts
+++ src/admin/routes/CustomerDiscountRouter.ts
@@ -4,21 +4,10 @@ import controller from '../controllers/CustomerDiscountController';
 
 const router = Router();
 
-router.get(
-  '/customers/:customerId/discounts',
-  controller.index.bind(controller)
-);
-router.post(
-  '/customers/:customerId/discounts',
-  controller.store.bind(controller)
-);
-router.put(
-  '/customers/:customerId/discounts',
-  controller.update.bind(controller)
-);
-router.delete(
-  '/customers/:customerId/discounts/:id',
-  controller.destroy.bind(controller)
-);
+router.get('/discounts', controller.index.bind(controller));
+router.get('/discounts/:id', controller.get.bind(controller));
+router.post('/discounts', controller.store.bind(controller));
+router.put('/discounts', controller.update.bind(controller));
+router.delete('/discounts/:id', controller.destroy.bind(controller));
 
 export default router;
