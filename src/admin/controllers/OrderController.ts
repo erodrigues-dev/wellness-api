b10fc8d71ceb8c294e097868cbdb18fbd08db2ef
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-11-03 22:54:26 -0300

    add order controller

--- src/admin/controllers/OrderController.ts
+++ src/admin/controllers/OrderController.ts
@@ -1,14 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import PayWithMoneyDTO from '../../shared/models/dto/PayWithMoneyDTO';
+import IOrderService from '../../shared/services/interfaces/IOrderService';
+import orderService from '../../shared/services/OrderService';
 
 export class OrderController {
+  constructor(private orderService: IOrderService) {}
+
   async payWithMoney(req: Request, res: Response, next: NextFunction) {
     try {
-      const { customerId, itemType, itemId } = req.body;
-      return res.json({ message: 'not implemented' });
+      const dto = new PayWithMoneyDTO().fromRequest(req.body);
+      dto.userId = req.user.id;
+      await this.orderService.payWithMoney(dto);
+      return res.status(StatusCodes.NO_CONTENT).json();
     } catch (error) {
       next(error);
     }
   }
 }
 
-export default new OrderController();
+export default new OrderController(orderService);
