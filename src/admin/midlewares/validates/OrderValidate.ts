154247d84acee0e5f128b93ff9f4d7d970a7b09d
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-11-11 20:15:51 -0300

    allow empty customerId

--- src/admin/midlewares/validates/OrderValidate.ts
+++ src/admin/midlewares/validates/OrderValidate.ts
@@ -1,6 +1,5 @@
 import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
-import { join } from 'path';
 
 const router = Router();
 
@@ -8,7 +7,7 @@ router.get(
   '/orders',
   celebrate({
     [Segments.QUERY]: Joi.object().keys({
-      customerId: Joi.number().optional(),
+      customerId: Joi.number().optional().allow(''),
       page: Joi.number().positive().optional().default(1),
       limit: Joi.number().positive().optional().default(10)
     })
