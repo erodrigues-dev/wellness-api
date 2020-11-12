62cd6dd2592517b61fe7d862d9b1a5b4155ec19f
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-11-11 22:22:06 -0300

    create axios error handler

--- src/shared/error-handlers/index.ts
+++ src/shared/error-handlers/index.ts
@@ -1,11 +1,13 @@
 import { Express } from 'express';
 
-import uploadErrorHandler from './UploadErrorHandler';
+import axiosErrorHandler from './AxiosErrorHandler';
 import customErrorHandler from './CustomErrorHandler';
 import jsonErrorHandler from './JsonErrorHandler';
+import uploadErrorHandler from './UploadErrorHandler';
 
 export default function useErrorHandlers(app: Express) {
   app.use(uploadErrorHandler);
   app.use(customErrorHandler);
+  app.use(axiosErrorHandler);
   app.use(jsonErrorHandler);
 }
