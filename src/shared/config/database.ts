b12e6d2c901c74618f53d4ee5e16b81be1fe9560
Elton Rodrigues <erodrigues.dev@gmail.com>
2020-12-14 20:29:38 -0300

    add DB_PORT variable in settings

--- src/shared/config/database.ts
+++ src/shared/config/database.ts
@@ -6,6 +6,7 @@ export const DB_CONFIG: Options = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 5432),
   define: {
     timestamps: true,
     underscored: true
