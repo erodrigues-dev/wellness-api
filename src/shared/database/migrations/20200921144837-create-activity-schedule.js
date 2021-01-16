18489a52cf05e2443ce62aa29df8346a3d9e430a
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-01-16 09:46:32 -0300

    fix migration error

--- src/shared/database/migrations/20200921144837-create-activity-schedule.js
+++ src/shared/database/migrations/20200921144837-create-activity-schedule.js
@@ -1,45 +1,47 @@
-export function up(queryInterface, Sequelize) {
-  return queryInterface.createTable('activities_schedules', {
-    id: {
-      primaryKey: true,
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      allowNull: false
-    },
-    activity_id: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: { model: 'activities' },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE'
-    },
-    title: { type: Sequelize.STRING(120), allowNull: false },
-    color: { type: Sequelize.STRING(7), allowNull: false },
-    date: { type: Sequelize.DATEONLY, allowNull: false },
-    start: { type: Sequelize.TIME, allowNull: false },
-    end: { type: Sequelize.TIME, allowNull: false },
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('activities_schedules', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false
+      },
+      activity_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'activities' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      title: { type: Sequelize.STRING(120), allowNull: false },
+      color: { type: Sequelize.STRING(7), allowNull: false },
+      date: { type: Sequelize.DATEONLY, allowNull: false },
+      start: { type: Sequelize.TIME, allowNull: false },
+      end: { type: Sequelize.TIME, allowNull: false },
 
-    recurrent: Sequelize.BOOLEAN,
-    repeat_every: Sequelize.INTEGER,
-    frequency: Sequelize.STRING(12),
-    weekdays: Sequelize.STRING(30),
-    ends_in: Sequelize.STRING(12),
-    until: Sequelize.DATEONLY,
-    ocurrences: Sequelize.INTEGER,
+      recurrent: Sequelize.BOOLEAN,
+      repeat_every: Sequelize.INTEGER,
+      frequency: Sequelize.STRING(12),
+      weekdays: Sequelize.STRING(30),
+      ends_in: Sequelize.STRING(12),
+      until: Sequelize.DATEONLY,
+      ocurrences: Sequelize.INTEGER,
 
-    created_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.fn('now')
-    },
-    updated_at: {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.fn('now')
-    }
-  });
-}
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
 
-export function down(queryInterface) {
-  return queryInterface.dropTable('activities_schedules');
-}
+  down: queryInterface => {
+    return queryInterface.dropTable('activities_schedules');
+  }
+};
