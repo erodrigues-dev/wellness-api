a6fddcb6cc4dce1b2b71c9ea20e8c669ae0d00bc
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-01-16 09:50:55 -0300

    feat: add migrations customer

---
+++ src/shared/database/migrations/20210116005530-email-confirmation-codes.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { QueryInterface, DataTypes } = require('sequelize');
+
+module.exports = {
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {DataTypes} Sequelize
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('email_confirmation_codes', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      code: {
+        type: Sequelize.STRING(36),
+        allowNull: false
+      },
+      created_at: { type: Sequelize.DATE, allowNull: false }
+    });
+
+    await queryInterface.addIndex('email_confirmation_codes', [
+      'email',
+      'code'
+    ]);
+  },
+  /**
+   * @param {QueryInterface} queryInterface
+   */
+  down: async queryInterface => {
+    await queryInterface.dropTable('email_confirmation_codes');
+  }
+};
