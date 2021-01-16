f3bdd3766a5800e9c0df1fb95626bcf401467c98
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-01-16 09:52:50 -0300

    adjust customer entity

---
+++ src/shared/models/entities/Customer.ts
@@ -0,0 +1,10 @@
+export class Customer {
+  id?: number;
+  name: string;
+  email: string;
+  password: string;
+  imageUrl: string;
+  squareId?: string;
+  privateNotes: string;
+  phone: string;
+}
