00fd8a2792179acef11727d74f891d99d31a371c
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-01-18 19:24:52 -0300

    update customer entity

--- src/shared/models/entities/Customer.ts
+++ src/shared/models/entities/Customer.ts
@@ -7,4 +7,32 @@ export class Customer {
   squareId?: string;
   privateNotes: string;
   phone: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  static map(db: any): Customer {
+    const entity = new Customer();
+
+    entity.id = db.id;
+    entity.name = db.name;
+    entity.email = db.email;
+    entity.password = db.password;
+    entity.imageUrl = db.imageUrl;
+    entity.squareId = db.squareId;
+    entity.privateNotes = db.privateNotes;
+    entity.phone = db.phone;
+    entity.createdAt = db.createdAt;
+    entity.updatedAt = db.updatedAt;
+
+    return entity;
+  }
+}
+
+export class CustomerCreateModel {
+  name: string;
+  email: string;
+  password: string;
+  imageUrl: string;
+  privateNotes: string;
+  phone: string;
 }
