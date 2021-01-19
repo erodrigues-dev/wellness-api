378e90c5177e2061063277cb502ba32893bc262b
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-01-18 22:16:33 -0300

    use SenddingBlueTemplate

--- src/shared/sendingblue/SendEmailConfirmation.ts
+++ src/shared/sendingblue/SendEmailConfirmation.ts
@@ -1,12 +1,15 @@
 import { AxiosInstance } from 'axios';
 
+import { SendingBlueTemplate } from './SendingBlueTemplate';
+
 export class SendEmailConfirmation {
   constructor(private api: AxiosInstance) {}
 
   async send(name: string, email: string, code: string): Promise<void> {
     await this.api.post('/', {
       to: [{ name, email }],
-      params: { code }
+      params: { code },
+      templateId: SendingBlueTemplate.EmailConfirmation.id
     });
   }
 }
