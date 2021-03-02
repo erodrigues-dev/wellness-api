9c0948775ae9a360a4b0266fd6af055b80c70dff
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-03-01 22:25:41 -0300

    feat: add catch validation error

--- src/shared/error-handlers/ValidatorErrorHandler.ts
+++ src/shared/error-handlers/ValidatorErrorHandler.ts
@@ -1,16 +1,19 @@
 import { isCelebrateError } from 'celebrate';
 import { NextFunction, Request, Response } from 'express';
 
-export default function (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export default function (err: any, req: Request, res: Response, next: NextFunction) {
   if (isCelebrateError(err)) {
     var validationError = [...err.details.values()][0];
     return res.status(400).json({
       message: validationError.message,
+      type: 'ValidationError',
+      status: 400
+    });
+  }
+
+  if (err.isJoi) {
+    return res.status(400).json({
+      message: err.message,
       type: 'ValidationError',
       status: 400
     });
