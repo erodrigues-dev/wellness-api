dfd7ff3a7a67ce4853148878fc8ccfad3ba932b0
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-04-15 21:19:29 -0300

    feat: create specialty service

---
+++ src/shared/services/SpecialtyService.ts
@@ -0,0 +1,43 @@
+import Specialty from '../database/models/Specialty';
+
+interface ListFilter {
+  page?: number;
+  limit?: number;
+}
+
+interface UpdateData {
+  id: number;
+  name: string;
+}
+
+export class SpecialtyService {
+  async list({ page = 1, limit = 10 }: ListFilter) {
+    return Specialty.findAndCountAll({
+      limit,
+      offset: (page - 1) * limit
+    });
+  }
+
+  async create(name: string) {
+    return Specialty.create({
+      name
+    });
+  }
+
+  async udpate(data: UpdateData) {
+    await Specialty.update(
+      {
+        name: data.name
+      },
+      {
+        where: { id: data.id }
+      }
+    );
+  }
+
+  async destroy(id: number) {
+    await Specialty.destroy({ where: { id } });
+  }
+}
+
+export default new SpecialtyService();
