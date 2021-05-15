3ad6826e74cb51c4998aa6e78780b9fb501e1f6f
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-05-15 16:04:36 -0300

    feat: (waiver) create add use case

---
+++ src/shared/useCases/customer/AddWaiverUseCase.ts
@@ -0,0 +1,27 @@
+import CustomError from '../../custom-error/CustomError';
+import CustomerWaiver from '../../database/models/CustomerWaiver';
+
+export class AddWaiverUseCase {
+  constructor(private customerId: number, private waiverId: number) {}
+
+  async handle() {
+    if (await this.hasWaiver()) throw new CustomError('Customer already has this waiver');
+
+    await this.addWaiver();
+  }
+
+  private async hasWaiver() {
+    const count = await CustomerWaiver.count({
+      where: { customerId: this.customerId, waiverId: this.waiverId }
+    });
+
+    return count > 0;
+  }
+
+  private async addWaiver() {
+    CustomerWaiver.create({
+      customerId: this.customerId,
+      waiverId: this.waiverId
+    });
+  }
+}
