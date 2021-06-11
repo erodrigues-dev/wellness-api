3bce27b2f38f9aeaf70f649bfe2e32f2d98b3e24
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-06-11 19:39:10 -0300

    refactor: separate logic to get paginate options

--- src/shared/useCases/customer/waiver/ListWaiversUseCase.ts
+++ src/shared/useCases/customer/waiver/ListWaiversUseCase.ts
@@ -2,13 +2,7 @@ import CustomerWaiver from '../../../database/models/CustomerWaiver';
 
 export class ListWaiverUseCase {
   async handle(customerId: number, page: number, limit: number) {
-    const hasPagination = Boolean(page && limit);
-    const paginateOptions = hasPagination
-      ? {
-          limit: limit,
-          offset: (page - 1) * limit
-        }
-      : {};
+    const paginateOptions = this.getPaginationOptions(page, limit);
     const result = await CustomerWaiver.findAndCountAll({
       where: { customerId },
       attributes: {
@@ -29,5 +23,16 @@ export class ListWaiverUseCase {
     });
 
     return result;
+  }
+
+  private getPaginationOptions(page: number, limit: number) {
+    const hasPagination = Boolean(page && limit);
+    if (hasPagination)
+      return {
+        limit: limit,
+        offset: (page - 1) * limit
+      };
+
+    return {};
   }
 }
