c0192548ed2897adde28e94b8ca5d037d15805a3
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-06-15 20:33:09 -0300

    refactor: use bindRoute util

--- src/site/routes/index.ts
+++ src/site/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import upload from '../../shared/utils/multer-google-cloud-storage';
+import { bindRoute } from '../../shared/utils/bindRoute';
 import {
   ProductDetailController,
   ProductListController,
@@ -39,49 +40,50 @@ const listSlotsController = makeListSlotsController();
 const bookController = makeBookController();
 
 //- sessions
-router.post('/sessions/signin', signinController.signin.bind(signinController));
-router.post('/sessions/recover-password', signinController.recoverPassword.bind(signinController));
-router.post('/sessions/signup', signupController.signup.bind(signupController));
-router.post('/sessions/send-confirmation-code', signupController.sendCode.bind(signupController));
+router.post('/sessions/signin', bindRoute(signinController, 'signin'));
+router.post('/sessions/recover-password', bindRoute(signinController, 'recoverPassword'));
+router.post('/sessions/signup', bindRoute(signupController, 'signup'));
+router.post('/sessions/send-confirmation-code', bindRoute(signupController, 'sendCode'));
 
 //- producs
-router.get('/products', productListController.handle.bind(productListController));
-router.get('/products/search', productSearchController.search.bind(productSearchController));
-router.get('/products/categories', productSearchController.listCategories.bind(productSearchController));
-router.get('/products/:id/:type', productDetailController.handle.bind(productDetailController));
+router.get('/products', bindRoute(productListController, 'handle'));
+router.get('/products/search', bindRoute(productSearchController, 'search'));
+router.get('/products/categories', bindRoute(productSearchController, 'listCategories'));
+router.get('/products/:id/:type', bindRoute(productDetailController, 'handle'));
 
 //- account
-router.get('/account', accountController.get.bind(accountController));
-router.put('/account', accountController.save.bind(accountController));
+router.get('/account', bindRoute(accountController, 'get'));
+router.put('/account', bindRoute(accountController, 'save'));
 router.put(
   '/account/change-image',
   upload(mimetypes).single('image'),
   accountController.changeImage.bind(accountController)
 );
-router.post('/account/generate-referral-code', accountController.generateReferralCode.bind(accountController));
+router.post('/account/generate-referral-code', bindRoute(accountController, 'generateReferralCode'));
 
 //- account/cards
-router.get('/account/cards', accountController.cards.bind(accountController));
-router.post('/account/cards', accountController.createCard.bind(accountController));
-router.delete('/account/cards/:card_id', accountController.deleteCard.bind(accountController));
+router.get('/account/cards', bindRoute(accountController, 'cards'));
+router.post('/account/cards', bindRoute(accountController, 'createCard'));
+router.delete('/account/cards/:card_id', bindRoute(accountController, 'deleteCard'));
 
 //- orders
-router.get('/orders', ordersController.list.bind(ordersController));
-router.get('/orders/:id', ordersController.get.bind(ordersController));
+router.get('/orders', bindRoute(ordersController, 'list'));
+router.get('/orders/:id', bindRoute(ordersController, 'get'));
 
 //- checkout
-router.post('/checkout', checkoutController.handle.bind(checkoutController));
-router.get('/checkout/discounts', getDiscountController.handle.bind(getDiscountController));
+router.post('/checkout', bindRoute(checkoutController, 'handle'));
+router.get('/checkout/discounts', bindRoute(getDiscountController, 'handle'));
 
 //- services
-router.get('/services', servicesListController.handle.bind(servicesListController));
+router.get('/services', bindRoute(servicesListController, 'handle'));
 
 // - appointments
-router.get('/appointments', appointmentsListController.handle.bind(appointmentsListController));
+router.get('/appointments', bindRoute(appointmentsListController, 'handle'));
 
 //- book
-router.get('/book/days', listDaysController.handle.bind(listDaysController));
-router.get('/book/slots', listSlotsController.handle.bind(listSlotsController));
-router.post('/book', bookController.handle.bind(bookController));
+router.get('/book/days', bindRoute(listDaysController, 'handle'));
+router.get('/book/slots', bindRoute(listSlotsController, 'handle'));
+router.post('/book', bindRoute(bookController, 'handle'));
+
 
 export default router;
