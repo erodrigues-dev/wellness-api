d2615e7cc8303f589a88eab715b671114199b9d8
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-06-22 21:54:04 -0300

    fix: permissions

--- src/admin/midlewares/permissions/WorkoutPermission.ts
+++ src/admin/midlewares/permissions/WorkoutPermission.ts
@@ -11,12 +11,16 @@ router.post('/workout-profiles', checkPermission(Permission.WorkoutProfileCreate
 router.put('/workout-profiles/:id', checkPermission(Permission.WorkoutProfileCreateUpdate));
 router.delete('/workout-profiles/:id', checkPermission(Permission.WorkoutProfileDelete));
 
-// router.use('/workout-profiles', checkPermission(Permission.WorkoutProfileList));
+router.get('/workout-logs', checkPermission(Permission.WorkoutProfileList));
+router.get('/workout-logs/:id', checkPermission(Permission.WorkoutProfileList));
+router.post('/workout-logs', checkPermission(Permission.WorkoutProfileCreateUpdate));
+router.put('/workout-logs/:id', checkPermission(Permission.WorkoutProfileCreateUpdate));
+router.delete('/workout-logs/:id', checkPermission(Permission.WorkoutProfileCreateUpdate));
 
-// router.delete('/workout-profiles/:id', checkPermission(Permission.WorkoutProfileDelete));
-
-// router.post('/workout-profiles/?*', checkPermission(Permission.WorkoutProfileCreateUpdate));
-// router.put('/workout-profiles/?*', checkPermission(Permission.WorkoutProfileCreateUpdate));
-// router.delete('/workout-profiles/?*', checkPermission(Permission.WorkoutProfileCreateUpdate));
+router.get('/workout-exercises', checkPermission(Permission.WorkoutProfileList));
+router.get('/workout-exercises/:id', checkPermission(Permission.WorkoutProfileList));
+router.post('/workout-exercises', checkPermission(Permission.WorkoutProfileCreateUpdate));
+router.put('/workout-exercises/:id', checkPermission(Permission.WorkoutProfileCreateUpdate));
+router.delete('/workout-exercises/:id', checkPermission(Permission.WorkoutProfileCreateUpdate));
 
 export default router;
