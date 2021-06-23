1150d4e27ca78fdfe763610fbc42283ce3457351
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-06-22 21:35:32 -0300

    refactor: improvement types

--- src/shared/utils/bindRoute.ts
+++ src/shared/utils/bindRoute.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const bindRoute = (controller: any, action: string) => {
+export const bindRoute = <T, K extends keyof T>(controller: T, action: K) => {
   if (!(action in controller)) throw new Error(`action: ${action} is not present in controller`);
 
-  return (req: Request, res: Response, next: NextFunction) => controller[action](req, res, next);
+  return (req: Request, res: Response, next: NextFunction) => controller[String(action)](req, res, next);
 };
