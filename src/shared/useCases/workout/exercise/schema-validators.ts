55c1dc8d69d122c622f3f54a5c05a97c93b489d0
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-07-03 15:32:25 -0300

    fix: exercise update schema validator

--- src/shared/useCases/workout/exercise/schema-validators.ts
+++ src/shared/useCases/workout/exercise/schema-validators.ts
@@ -16,7 +16,7 @@ export const createSchema = Joi.object({
 
 export const updateSchema = Joi.object({
   id: Joi.number().required(),
-  name: Joi.string().required(),
+  name: Joi.string().min(1).trim(),
   notes: Joi.string().allow(null),
   set1Reps: Joi.number().allow(null),
   set1Weight: Joi.number().allow(null),
