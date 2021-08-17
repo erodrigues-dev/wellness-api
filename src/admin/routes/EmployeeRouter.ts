2ab4a7da85cc832f54765306663ec024dbfef733
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-08-17 20:24:52 -0300

    fix: remove notification routes from employee router

--- src/admin/routes/EmployeeRouter.ts
+++ src/admin/routes/EmployeeRouter.ts
@@ -2,15 +2,8 @@ import { Router } from 'express';
 import { bindRoute } from '../../shared/utils/bindRoute';
 
 import controller from '../controllers/EmployeeController';
-import notificationController from '../controllers/NotificationController';
 
 const router = Router();
-
-router.get('/employees/notifications', bindRoute(notificationController, 'listByEmployee'));
-router.put('/employees/notifications/:id/mark-as-read', bindRoute(notificationController, 'markAsRead'));
-router.put('/employees/notifications/:id/mark-as-unread', bindRoute(notificationController, 'markAsUnread'));
-router.put('/employees/notifications/mark-all-as-read', bindRoute(notificationController, 'markAllAsRead'));
-router.put('/employees/notifications/mark-all-as-unread', bindRoute(notificationController, 'markAllAsUnread'));
 
 router.get('/employees', bindRoute(controller, 'index'));
 router.get('/employees/:id', bindRoute(controller, 'get'));
