67c3d603a4ed59e65443e46cb478aa4597b1ce13
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-08-17 20:25:30 -0300

    fix: unify permission of notifications in router module

--- src/admin/routes/NotificationRouter.ts
+++ src/admin/routes/NotificationRouter.ts
@@ -2,11 +2,20 @@ import { Router } from 'express';
 import { bindRoute } from '../../shared/utils/bindRoute';
 import controller from '../controllers/NotificationController';
 
+import { Permission } from '../../shared/models/entities/Permission';
+import { checkPermission } from '../../shared/utils/permission';
+
 const router = Router();
 
-router.get('/notifications', bindRoute(controller, 'index'));
-router.get('/notifications/:id', bindRoute(controller, 'get'));
-router.post('/notifications', bindRoute(controller, 'store'));
-router.delete('/notifications/:id', bindRoute(controller, 'destroy'));
+router.get('/notifications/by-employee', bindRoute(controller, 'listByEmployee'));
+router.put('/notifications/:id/mark-as-read', bindRoute(controller, 'markAsRead'));
+router.put('/notifications/:id/mark-as-unread', bindRoute(controller, 'markAsUnread'));
+router.put('/notifications/mark-all-as-read', bindRoute(controller, 'markAllAsRead'));
+router.put('/notifications/mark-all-as-unread', bindRoute(controller, 'markAllAsUnread'));
+
+router.get('/notifications', checkPermission(Permission.NotificationList), bindRoute(controller, 'index'));
+router.get('/notifications/:id', checkPermission(Permission.NotificationList), bindRoute(controller, 'get'));
+router.post('/notifications', checkPermission(Permission.NotificationCreateUpdate), bindRoute(controller, 'store'));
+router.delete('/notifications/:id', checkPermission(Permission.NotificationDelete), bindRoute(controller, 'destroy'));
 
 export default router;
