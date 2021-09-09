c95d0bacfd812ea420b41f49f8437e74ad0ae10c
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-09-08 21:01:52 -0300

    feat: add destroy route and use bindRoute helper

--- src/admin/routes/PackageRouter.ts
+++ src/admin/routes/PackageRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import { bindRoute } from '../../shared/utils/bindRoute';
 import controller from '../controllers/PackageController';
 
 const router = Router();
-router.get('/packages', controller.index.bind(controller));
-router.get('/packages/:id', controller.get.bind(controller));
-router.post('/packages', controller.store.bind(controller));
-router.put('/packages', controller.update.bind(controller));
-// router.delete('/packages/:id', controller.destroy.bind(controller))
+router.get('/packages', bindRoute(controller, 'index'));
+router.get('/packages/:id', bindRoute(controller, 'get'));
+router.post('/packages', bindRoute(controller, 'store'));
+router.put('/packages', bindRoute(controller, 'update'));
+router.delete('/packages/:id', bindRoute(controller, 'destroy'));
 
 export default router;
