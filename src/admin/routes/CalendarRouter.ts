d2d3e60ea7e0963d5ddb0327bfe2c83394b7196e
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-09-29 21:19:13 -0300

    feature: check permissions

--- src/admin/routes/CalendarRouter.ts
+++ src/admin/routes/CalendarRouter.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import { bindRoute } from '../../shared/utils/bindRoute';
 
+import { Permission } from '../../shared/models/entities/Permission';
+import { checkPermission } from '../../shared/utils/permission';
+
 import { makeCalendarController } from '../controllers/CalendarController';
 
 const controller = makeCalendarController();
 const router = Router();
 
-router.get('/calendars', bindRoute(controller, 'index'));
-router.get('/calendars/:id', bindRoute(controller, 'get'));
-router.post('/calendars', bindRoute(controller, 'store'));
-router.put('/calendars/:id', bindRoute(controller, 'update'));
-router.delete('/calendars/:id', bindRoute(controller, 'destroy'));
+router.get('/calendars', checkPermission(Permission.CalendarList), bindRoute(controller, 'index'));
+router.get('/calendars/:id', checkPermission(Permission.CalendarList), bindRoute(controller, 'get'));
+router.post('/calendars', checkPermission(Permission.CalendarCreateUpdate), bindRoute(controller, 'store'));
+router.put('/calendars/:id', checkPermission(Permission.CalendarCreateUpdate), bindRoute(controller, 'update'));
+router.delete('/calendars/:id', checkPermission(Permission.CalendarDelete), bindRoute(controller, 'destroy'));
 
 export default router;
