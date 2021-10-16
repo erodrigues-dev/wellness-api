0ed28b285826eb65d31d7381b3cbb8947d611d11
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-10-15 21:02:43 -0300

    feat: create use cases to list and store slots

---
+++ src/shared/useCases/calendar/slots/CalendarSlotListUseCase.ts
@@ -0,0 +1,21 @@
+import Model from '../../../database/models/CalendarSlot';
+
+export class CalendarSlotListUseCase {
+  async list(calendarId) {
+    const dbList = await Model.findAll({
+      where: { calendarId }
+    });
+
+    return dbList.map(this.parse);
+  }
+
+  private parse(item: Model) {
+    return {
+      id: item.id,
+      start: item.start,
+      end: item.end,
+      recurrenceRule: item.recurrenceRule,
+      recurrenceExceptions: JSON.parse(item.recurrenceExceptions || '[]')
+    };
+  }
+}
