5b50feabdadd1f27c0e057ec1a6e25076f8dc9b7
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-12-13 19:03:13 -0300

    feat: create team group permissions

--- src/admin/routes/TeamGroupRouter.ts
+++ src/admin/routes/TeamGroupRouter.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import { bindRoute } from '../../shared/utils/bindRoute';
 
+import { Permission } from '../../shared/models/entities/Permission';
+import { checkPermission } from '../../shared/utils/permission';
+
 import { makeTeamGroupController } from '../controllers/TeamGroupController';
 
 const router = Router();
 const controller = makeTeamGroupController();
 
-router.get('/team-groups', bindRoute(controller, 'index'));
-router.get('/team-groups/:id', bindRoute(controller, 'get'));
-router.post('/team-groups', bindRoute(controller, 'create'));
-router.put('/team-groups/:id', bindRoute(controller, 'update'));
-router.delete('/team-groups/:id', bindRoute(controller, 'destroy'));
+router.get('/team-groups', checkPermission(Permission.TeamGroupList), bindRoute(controller, 'index'));
+router.get('/team-groups/:id', checkPermission(Permission.TeamGroupList), bindRoute(controller, 'get'));
+router.post('/team-groups', checkPermission(Permission.TeamGroupCreateUpdate), bindRoute(controller, 'create'));
+router.put('/team-groups/:id', checkPermission(Permission.TeamGroupCreateUpdate), bindRoute(controller, 'update'));
+router.delete('/team-groups/:id', checkPermission(Permission.TeamGroupDelete), bindRoute(controller, 'destroy'));
 
 export default router;
