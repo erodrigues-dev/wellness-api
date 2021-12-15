81978075887bfe1f614be8efb87533d1657966db
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-12-15 19:11:38 -0300

    feat: auto complete team groups

--- src/admin/controllers/AutoCompleteController.ts
+++ src/admin/controllers/AutoCompleteController.ts
@@ -2,19 +2,35 @@ import { Request, Response, NextFunction } from 'express';
 
 import { Op } from 'sequelize';
 import Customer from '../../shared/database/models/Customer';
+import TeamGroup from '../../shared/database/models/TeamGroup';
+
+const LIMIT = 20;
 
 export class AutoCompleteControlller {
   async customers(req: Request, res: Response, next: NextFunction) {
     try {
       const { q = '' } = req.query as any;
-
       const list = await Customer.findAll({
-        where: {
-          name: { [Op.iLike]: `%${q}%` }
-        },
+        where: { name: { [Op.iLike]: `%${q}%` } },
         attributes: ['id', 'name'],
         order: ['name'],
-        limit: 20
+        limit: LIMIT
+      });
+
+      return res.json(list);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async teamGroups(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { q = '' } = req.query as any;
+      const list = await TeamGroup.findAll({
+        where: { name: { [Op.iLike]: `%${q}%` } },
+        attributes: ['id', 'name'],
+        order: ['name'],
+        limit: LIMIT
       });
 
       return res.json(list);
