d97a119e845a04bd37a77c9568656c5b43b4155c
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-01-04 21:13:39 -0300

    feat: create autocomplete for employees

--- src/admin/controllers/AutoCompleteController.ts
+++ src/admin/controllers/AutoCompleteController.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import { Op } from 'sequelize';
 import Customer from '../../shared/database/models/Customer';
 import TeamGroup from '../../shared/database/models/TeamGroup';
+import Employee from '../../shared/database/models/Employee';
 
 const LIMIT = 20;
 
@@ -34,6 +35,42 @@ export class AutoCompleteControlller {
       });
 
       return res.json(list);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async employees(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { q, specialty } = req.query as any;
+
+      const query: any[] = [];
+
+      if (q) {
+        query.push({ name: { [Op.iLike]: `%${q}%` } });
+      }
+
+      if (specialty) {
+        query.push({ '$specialty.name$': specialty });
+      }
+
+      const list = await Employee.findAll({
+        where: { [Op.and]: query },
+        attributes: ['id', 'name'],
+        include: {
+          association: 'specialty',
+          attributes: ['name']
+        },
+        order: ['name'],
+        limit: LIMIT
+      });
+
+      const parsed = list.map(item => ({
+        id: item.id,
+        name: item.name
+      }));
+
+      return res.json(parsed);
     } catch (error) {
       next(error);
     }
