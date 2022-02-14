9e3a13638ea9901c09d0d337f72c78965117e971
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-14 19:19:13 -0300

    refactor: improve dotenv load

--- dotenv-config.js
+++ dotenv-config.js
@@ -2,11 +2,15 @@ const { config } = require('dotenv');
 const fs = require('fs');
 
 const NODE_ENV = process.env.NODE_ENV;
-const dotEnvExist = fs.existsSync(`.env.${NODE_ENV}`);
 
-console.log(`>> dotEnv load ${NODE_ENV}`);
-if (!dotEnvExist) {
-  console.log(`.env.${NODE_ENV} not found trying load .env`);
+let dotEnvExist = false;
+
+if (NODE_ENV) {
+  console.log(`>> dotEnv load ${NODE_ENV}`);
+  dotEnvExist = fs.existsSync(`.env.${NODE_ENV}`);
+  if (!dotEnvExist) {
+    console.log(`.env.${NODE_ENV} not found trying load .env`);
+  }
 }
 
 config({
