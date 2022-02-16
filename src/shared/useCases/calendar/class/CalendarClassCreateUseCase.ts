0028cc20da529f9b6faaa0f119f93ef92209243d
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-15 21:49:28 -0300

    feat: validate and calculate dateEnd

--- src/shared/useCases/calendar/class/CalendarClassCreateUseCase.ts
+++ src/shared/useCases/calendar/class/CalendarClassCreateUseCase.ts
@@ -1,14 +1,35 @@
+import Activity from '../../../database/models/Activity'
 import CalendarClass from '../../../database/models/CalendarClass'
 
-import { storeSchema } from './schema'
+import { parseISO, addMinutes } from '../../../utils/date-utils'
+
+import { storeSchema } from './calendar-class-schema'
+
+interface Data {
+  calendarId: string
+  activityId: number
+  dateStart: string
+  slots: number
+  recurrenceRule: string
+  recurrenceExceptions: string
+  color: string
+  notes: string
+}
 
 export class CalendarClassCreateUseCase {
-  async handle(data: any) {
+  async handle(data: Data) {
     await this.validate(data)
-    return CalendarClass.create(data)
+    const dateEnd = await this.calculateEndDate(data)
+    return CalendarClass.create({ ...data, dateEnd })
   }
 
-  validate(data: any) {
-    return storeSchema.validateAsync(data)
+  async validate(data: Data) {
+    await storeSchema.validateAsync(data)
+  }
+
+  async calculateEndDate({ activityId, dateStart }: Data) {
+    const { duration } = await Activity.findByPk(activityId, { attributes: ['duration'] })
+    const date = parseISO(dateStart)
+    return addMinutes(date, duration)
   }
 }
