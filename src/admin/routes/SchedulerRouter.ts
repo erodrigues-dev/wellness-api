219aac06ff30658895ee15b4cbc3284d33d368e8
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-17 22:57:20 -0300

    feat: controller and routes

--- src/admin/routes/SchedulerRouter.ts
+++ src/admin/routes/SchedulerRouter.ts
@@ -1,18 +1,33 @@
-import { Router } from 'express';
-import { bindRoute } from '../../shared/utils/bindRoute';
-import { makeSchedulerController } from '../controllers/SchedulerController';
+import { Router } from 'express'
+import { bindRoute } from '../../shared/utils/bindRoute'
+import { makeCalendarClassAppointmentController } from '../controllers/CalendarClassAppointmentController'
+import { makeSchedulerController } from '../controllers/SchedulerController'
 
-const controller = makeSchedulerController();
-const router = Router();
+const controller = makeSchedulerController()
+const calendarClassAppointmentController = makeCalendarClassAppointmentController()
+const router = Router()
 
-router.get('/scheduler/calendars', bindRoute(controller, 'calendars'));
-router.get('/scheduler/calendars/:id/activities', bindRoute(controller, 'activities'));
-router.get('/scheduler/slots', bindRoute(controller, 'slots'));
+router.get('/scheduler/calendars', bindRoute(controller, 'calendars'))
+router.get('/scheduler/calendars/:id/activities', bindRoute(controller, 'activities'))
+router.get('/scheduler/slots', bindRoute(controller, 'slots'))
 
-router.get('/scheduler/items', bindRoute(controller, 'listItems'));
-router.get('/scheduler/items/:id', bindRoute(controller, 'getItem'));
-router.post('/scheduler/items', bindRoute(controller, 'addItem'));
-router.put('/scheduler/items/:id', bindRoute(controller, 'updateItem'));
-router.post('/scheduler/items/:id/cancel', bindRoute(controller, 'cancelItem'));
+router.get('/scheduler/items', bindRoute(controller, 'listItems'))
+router.get('/scheduler/items/:id', bindRoute(controller, 'getItem'))
+router.post('/scheduler/items', bindRoute(controller, 'addItem'))
+router.put('/scheduler/items/:id', bindRoute(controller, 'updateItem'))
+router.post('/scheduler/items/:id/cancel', bindRoute(controller, 'cancelItem'))
 
-export default router;
+router.post(
+  '/scheduler/classes/appointments',
+  bindRoute(calendarClassAppointmentController, 'store')
+)
+router.put(
+  '/scheduler/classes/appointments/:id',
+  bindRoute(calendarClassAppointmentController, 'update')
+)
+router.post(
+  '/scheduler/classes/appointments/:id/cancel',
+  bindRoute(calendarClassAppointmentController, 'cancel')
+)
+
+export default router
