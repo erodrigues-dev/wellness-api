dcbb3d0147b2a8b6e5d47b389936fc1b1e595e72
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-21 21:08:41 -0300

    feat: create calendar add appointment use case

---
+++ src/shared/useCases/calendar/appointment/AddAppointment.ts
@@ -0,0 +1,76 @@
+import CalendarAppointment from '../../../database/models/CalendarAppointment'
+import Activity from '../../../database/models/Activity'
+
+import { addMinutes } from '../../../utils/date-utils'
+
+import { createAppointmentSchema } from './schema'
+import CalendarClass from '../../../database/models/CalendarClass'
+
+interface AddAppointmentData {
+  calendarClassId: string
+  calendarId: string
+  activityId: number
+  dateStart: string
+  customerId: number
+  calendarLabelId: string
+  notes: string
+}
+
+export class CalendarAddAppointmentUseCase {
+  async handle(data: AddAppointmentData) {
+    await createAppointmentSchema.validateAsync(data)
+    const id = await this.save(data)
+    return this.getModel(id)
+  }
+
+  private save(data: AddAppointmentData) {
+    const isClassAppointment = Boolean(data.calendarClassId)
+    if (isClassAppointment) return this.saveWithClassAppointment(data)
+    return this.saveWithAppointment(data)
+  }
+
+  private async saveWithClassAppointment(data: AddAppointmentData) {
+    const calendarClass = await CalendarClass.findByPk(data.calendarClassId)
+
+    const dateEnd = await this.calculateDateEnd({
+      dateStart: data.dateStart,
+      activityId: calendarClass.activityId
+    })
+
+    const appointmentData = {
+      dateStart: data.dateStart,
+      dateEnd,
+      calendarClassId: calendarClass.id,
+      activityId: calendarClass.activityId,
+      customerId: data.customerId,
+      calendarLabelId: data.calendarLabelId,
+      notes: data.notes
+    }
+
+    const { id } = await CalendarAppointment.create(appointmentData)
+    return id
+  }
+
+  private async saveWithAppointment(data: AddAppointmentData) {
+    const dateEnd = await this.calculateDateEnd(data)
+
+    const { id } = await CalendarAppointment.create({ ...data, dateEnd })
+    return id
+  }
+
+  private async calculateDateEnd({ dateStart, activityId }) {
+    const { duration } = await Activity.findByPk(activityId)
+    return addMinutes(new Date(dateStart), duration)
+  }
+
+  private getModel(id) {
+    return CalendarAppointment.findByPk(id, {
+      include: [
+        { association: 'calendar', attributes: ['id', 'name'] },
+        { association: 'activity', attributes: ['id', 'name', 'duration'] },
+        { association: 'customer', attributes: ['id', 'name'] },
+        { association: 'calendarLabel', attributes: ['id', 'name', 'color'] }
+      ]
+    })
+  }
+}
