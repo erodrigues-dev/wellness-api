8caa5b3de62757e2525afc0bfbba2611ecf715b1
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-21 21:09:01 -0300

    feat: create appointment schemas

---
+++ src/shared/useCases/calendar/appointment/schema.ts
@@ -0,0 +1,28 @@
+import Joi from 'joi'
+
+export const createAppointmentSchema = Joi.object({
+  calendarClassId: Joi.string().uuid().allow(null),
+  calendarId: Joi.when('calendarClassId', {
+    is: Joi.string().required(),
+    then: Joi.string().uuid().allow(null),
+    otherwise: Joi.string().uuid().required()
+  }),
+  activityId: Joi.when('calendarClassId', {
+    is: Joi.string().required(),
+    then: Joi.number().allow(null),
+    otherwise: Joi.number().required()
+  }),
+  dateStart: Joi.string().isoDate().required(),
+  customerId: Joi.number().required(),
+  calendarLabelId: Joi.string().uuid().allow(null),
+  notes: Joi.string().max(600).allow(null, '')
+})
+
+export const updateAppointmentSchema = createAppointmentSchema.append({
+  id: Joi.string().uuid().required()
+})
+
+export const listAppointmentsSchema = Joi.object({
+  calendars: Joi.array().items(Joi.string()).required().min(1),
+  date: Joi.string().isoDate().required()
+})
