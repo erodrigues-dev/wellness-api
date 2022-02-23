dc4c32fb9511214f12398d73addfa0436e04e2e1
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-22 22:43:48 -0300

    style: add lines separator

--- src/admin/routes/SchedulerRouter.ts
+++ src/admin/routes/SchedulerRouter.ts
@@ -7,10 +7,12 @@ const router = Router()
 
 router.get('/scheduler/calendars', bindRoute(controller, 'calendars'))
 router.get('/scheduler/calendars/:id/activities', bindRoute(controller, 'activities'))
+
 router.get('/scheduler/slots', bindRoute(controller, 'slots'))
 
 router.get('/scheduler/items', bindRoute(controller, 'listItems'))
 router.get('/scheduler/items/:id', bindRoute(controller, 'getItem'))
+
 router.post('/scheduler/appointments', bindRoute(controller, 'addAppointment'))
 router.put('/scheduler/appointments/:id', bindRoute(controller, 'updateAppointment'))
 router.post(
