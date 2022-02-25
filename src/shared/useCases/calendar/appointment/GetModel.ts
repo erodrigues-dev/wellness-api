a253918c9058edf8823d7943b96e6b8e6d0a56fe
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-02-25 18:54:21 -0300

    feat: return includes

--- src/shared/useCases/calendar/appointment/GetModel.ts
+++ src/shared/useCases/calendar/appointment/GetModel.ts
@@ -3,12 +3,16 @@ import CalendarAppointment from '../../../database/models/CalendarAppointment'
 export class GetModel {
   handle(id) {
     return CalendarAppointment.findByPk(id, {
-      include: [
-        { association: 'calendar', attributes: ['id', 'name'] },
-        { association: 'activity', attributes: ['id', 'name', 'duration'] },
-        { association: 'customer', attributes: ['id', 'name'] },
-        { association: 'calendarLabel', attributes: ['id', 'name', 'color'] }
-      ]
+      include: this.getIncludes()
     })
+  }
+
+  getIncludes() {
+    return [
+      { association: 'calendar', attributes: ['id', 'name'] },
+      { association: 'activity', attributes: ['id', 'name', 'duration'] },
+      { association: 'customer', attributes: ['id', 'name'] },
+      { association: 'calendarLabel', attributes: ['id', 'name', 'color'] }
+    ]
   }
 }
