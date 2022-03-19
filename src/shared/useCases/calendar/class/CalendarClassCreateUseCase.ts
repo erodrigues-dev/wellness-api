d6c4e9cbe6abee6a4fb3ee852afbec4d7b4aa2ef
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-03-19 15:21:00 -0300

    feat: expand recurrence class on create

--- src/shared/useCases/calendar/class/CalendarClassCreateUseCase.ts
+++ src/shared/useCases/calendar/class/CalendarClassCreateUseCase.ts
@@ -1,10 +1,11 @@
-import Activity from '../../../database/models/Activity'
+import RRule from 'rrule'
+import { v4 as uuid } from 'uuid'
+
 import CalendarClass from '../../../database/models/CalendarClass'
-
-import { parseISO, addMinutes } from '../../../utils/date-utils'
-
 import { storeSchema } from './schema'
 import { GetModel } from './GetModel'
+import { CalculateDateEnd } from '../shared/CalculateDateEnd'
+import { addMinutes, parseISO, startOfDay } from '../../../utils/date-utils'
 
 interface Data {
   calendarId: string
@@ -18,15 +19,16 @@ interface Data {
 
 export class CalendarClassCreateUseCase {
   private getModel: GetModel
+  private calculateDateEnd: CalculateDateEnd
 
   constructor() {
     this.getModel = new GetModel()
+    this.calculateDateEnd = new CalculateDateEnd()
   }
 
   async handle(data: Data) {
     await this.validate(data)
-    const dateEnd = await this.calculateEndDate(data)
-    const { id } = await CalendarClass.create({ ...data, dateEnd })
+    const { id } = await this.save(data)
     return this.getModel.handle(id)
   }
 
@@ -34,21 +36,46 @@ export class CalendarClassCreateUseCase {
     await storeSchema.validateAsync(data)
   }
 
-  async save(data) {
-    const endDate = await this.calculateEndDate(data)
-    const model = await CalendarClass.create({ ...data, endDate })
-    await model.reload({
-      include: [
-        { association: 'calendar', attributes: ['id', 'name'] },
-        { association: 'activity', attributes: ['id', 'name', 'duration'] }
-      ]
-    })
+  async save(data: Data) {
+    if (data.recurrenceRule) return await this.saveRecurrence(data)
+    return await this.saveUnique(data)
+  }
+
+  async saveUnique(data: Data) {
+    const dateEnd = await this.calculateDateEnd.calculate(data)
+    const model = await CalendarClass.create({ ...data, dateEnd })
     return model
   }
 
-  async calculateEndDate({ activityId, dateStart }: Data) {
-    const { duration } = await Activity.findByPk(activityId, { attributes: ['duration'] })
-    const date = parseISO(dateStart)
-    return addMinutes(date, duration)
+  async saveRecurrence(data: Data) {
+    const originalDateStart = parseISO(data.dateStart)
+
+    const rule = RRule.fromString(data.recurrenceRule)
+    rule.options.dtstart = startOfDay(originalDateStart)
+    rule.options.byhour = [originalDateStart.getUTCHours()]
+    rule.options.byminute = [originalDateStart.getMinutes()]
+    rule.options.bysecond = [0]
+
+    const maxLength180 = (_date: Date, length: number) => length <= 180
+
+    const allDates = rule.all(maxLength180)
+
+    const recurrenceId = uuid()
+    const activityDuration = await this.calculateDateEnd.getDuration(data.activityId)
+
+    const models = await Promise.all(
+      allDates.map(dateStart => {
+        const dateEnd = addMinutes(dateStart, activityDuration)
+
+        return CalendarClass.create({
+          ...data,
+          dateStart,
+          dateEnd,
+          recurrenceId
+        })
+      })
+    )
+
+    return models[0]
   }
 }
