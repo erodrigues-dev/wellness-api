71ee38112ff8f466e794a78dc4034ccbdb6622a2
Elton Rodrigues <erodrigues.dev@gmail.com>
2022-03-23 20:57:01 -0300

    fix: query by recurrent items

--- src/shared/useCases/calendar/scheduler/SchedulerListSlotsUseCase.ts
+++ src/shared/useCases/calendar/scheduler/SchedulerListSlotsUseCase.ts
@@ -1,5 +1,6 @@
-import { Op } from 'sequelize'
-import Model from '../../../database/models/CalendarSlot'
+import { fn, literal, Op } from 'sequelize'
+import CalendarSlot from '../../../database/models/CalendarSlot'
+import { getDate } from '../../../utils/date-utils'
 import { RecurrenceUtil } from '../../../utils/RecurrenceUtil'
 import { GetModel } from '../slots/GetModel'
 
@@ -15,21 +16,48 @@ export class SchedulerListSlotUseCase {
   ) {}
 
   async handle(data: Data) {
-    const dbList = await Model.findAll({
+    const [byDate, recurrents] = await Promise.all([
+      this.queryByDate(data),
+      this.queryRecurrentItems(data)
+    ])
+
+    return [...byDate, ...recurrents].map(item => this.getModel.map(item))
+  }
+
+  private async queryByDate(data: Data) {
+    const dateOnly = getDate(data.date)
+    const dbList = await CalendarSlot.findAll({
       where: {
-        calendarId: { [Op.in]: data.calendars }
+        [Op.and]: [
+          {
+            calendarId: { [Op.in]: data.calendars },
+            recurrenceRule: { [Op.is]: null }
+          },
+          literal(`date_trunc('day', "CalendarSlot"."start") = '${dateOnly}'`)
+        ]
       },
       include: this.getModel.getIncludes()
     })
 
     return dbList
-      .filter(item =>
-        this.recurrenceUtil.hasDateInRecurrence({
-          rrule: item.recurrenceRule,
-          exceptions: item.recurrenceExceptions,
-          date: data.date
-        })
-      )
-      .map(item => this.getModel.map(item))
+  }
+
+  private async queryRecurrentItems(data: Data) {
+    const dbList = await CalendarSlot.findAll({
+      where: {
+        calendarId: { [Op.in]: data.calendars },
+        start: { [Op.lte]: data.date },
+        recurrenceRule: { [Op.not]: null }
+      },
+      include: this.getModel.getIncludes()
+    })
+
+    return dbList.filter(item =>
+      this.recurrenceUtil.hasDateInRecurrence({
+        rrule: item.recurrenceRule,
+        exceptions: item.recurrenceExceptions,
+        date: data.date
+      })
+    )
   }
 }
